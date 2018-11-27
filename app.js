8bafbd6b5c68c62c8f147fd33b5f1e644dd2a32d
T-Wesst <code.daddy17@gmail.com>
2018-11-26 22:15:06 -0800

    arrow func

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port, process.env.IP, function() {
-  console.log(`The App server has started on port ${port}`);
-});
+app.listen(port, process.env.IP, () =>
+  console.log(`The App server has started on port ${port}`)
+);
