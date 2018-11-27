e5396a05857ca26e223190b0d3fa0976bf5eff22
T-Wesst <code.daddy17@gmail.com>
2018-11-26 22:24:57 -0800

    create root path

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+// set template
+app.set("view engine", "ejs");
+// root path
+app.get("/", function(req, res) {
+  res.send("this will be the landing page soon");
+});
 
 app.listen(port, process.env.IP, () =>
   console.log(`The App server has started on port ${port}`)
