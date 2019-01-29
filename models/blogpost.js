0131b2f38182cafa1b3a96517352b6679e9dbd17
T-Wesst <code.daddy17@gmail.com>
2019-01-28 22:08:42 -0800

    restructure blogpost model

--- models/blogpost.js
+++ models/blogpost.js
@@ -1,24 +1,40 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 //Schema Setup
-var blogpostSchema = new mongoose.Schema({
-    title: String,
-    price: String,
-    image: String,
-    body: String,
-    createdAt: {type: Date, default: Date.now},
-    author: {
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
-        },
-        username: String
+blogpostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: 'Name cannot be blank',
+    trim: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now
+  },
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
     },
-    comments: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Comment'
-        }
-    ]
+    username: String
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment'
+    }
+  ]
 });
 
 module.exports = mongoose.model('Blogpost', blogpostSchema);
