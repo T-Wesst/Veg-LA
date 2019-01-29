dad8b4a158952cf003eabc8bcbb7bf06e6a99671
T-Wesst <code.daddy17@gmail.com>
2019-01-28 22:29:12 -0800

    add const and let

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-const express = require("express"),
+const express = require('express'),
   app = express(),
-  bodyParser = require("body-parser"),
-  mongoose = require("mongoose"),
-  flash = require("connect-flash"),
-  Blogpost = require("./models/blogpost"),
-  passport = require("passport"),
-  LocalStrategy = require("passport-local"),
-  methodOverride = require("method-override"),
-  Comment = require("./models/comment"),
-  User = require("./models/user"),
-  seedDB = require("./seeds"),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  flash = require('connect-flash'),
+  Blogpost = require('./models/blogpost'),
+  passport = require('passport'),
+  LocalStrategy = require('passport-local'),
+  methodOverride = require('method-override'),
+  Comment = require('./models/comment'),
+  User = require('./models/user'),
+  seedDB = require('./seeds'),
   port = process.env.PORT || 3000;
 
 //REQUIRING ROUTES
-const commentRoutes = require("./routes/comments"),
-  blogpostRoutes = require("./routes/blogposts"),
-  indexRoutes = require("./routes/index");
+const commentRoutes = require('./routes/comments'),
+  blogpostRoutes = require('./routes/blogposts'),
+  indexRoutes = require('./routes/index');
 
 //env var deployment || local
-const url = process.env.DATABASEURL || "mongodb://localhost/BlogApp";
+const url = process.env.DATABASEURL || 'mongodb://localhost/BlogApp';
 mongoose.connect(
   url,
   { useNewUrlParser: true }
 );
 
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 app.use(flash());
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 // seedDB(); seeds the database
 
 //MomentJS used in all view files via var moment
-app.locals.moment = require("moment");
+app.locals.moment = require('moment');
 
 //PASSPORT CONFIGURATION
 app.use(
-  require("express-session")({
-    secret: "this text can be anything",
+  require('express-session')({
+    secret: 'this text can be anything',
     resave: false,
     saveUninitialized: false
   })
@@ -52,14 +52,14 @@ passport.deserializeUser(User.deserializeUser());
 // middleware called on every route and template
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
-  res.locals.error = req.flash("error");
-  res.locals.success = req.flash("success");
+  res.locals.error = req.flash('error');
+  res.locals.success = req.flash('success');
   next();
 });
 
-app.use("/", indexRoutes);
-app.use("/blogposts/:id/comments", commentRoutes);
-app.use("/blogposts", blogpostRoutes);
+app.use('/', indexRoutes);
+app.use('/blogposts/:id/comments', commentRoutes);
+app.use('/blogposts', blogpostRoutes);
 
 app.listen(port, process.env.IP, () =>
   console.log(`The server is running on port ${port}`)
